3e7116a771c23b88a8dbd78fa80f430f2e79e5f8
Albert <kamalov00albert@gmail.com>
2023-07-31 09:15:13 +0300

    Убрал комментарий с медиазапросами

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -79,20 +79,3 @@ const App = () => {
 
 export default App;
 
-/*
-
-    @include mq('custom', 'max', 1024) {
-
-    }
-
-    @include mq('custom', 'max', 768) {
-
-    }
-
-    @include mq('custom', 'max', 640) {
-
-    }
-
-*/
-
-
